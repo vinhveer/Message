8f05184571ce000ebd29a27b06bd0948dd1423fc
vinhveer <63202399+vinhveer@users.noreply.github.com>
2024-03-31 13:54:53 +0700

    first commit

---
+++ src/test/java/org/vinhveer/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vinhveer.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
