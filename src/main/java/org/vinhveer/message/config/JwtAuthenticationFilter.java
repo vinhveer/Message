af44af1b8d06214f99c2fac2447b6da449dab444
VINHVEER\vinhv <vinhveer123@gmail.com>
2024-04-29 12:12:08 +0700

    change Config file name

--- src/main/java/org/vinhveer/message/config/JwtAuthenticationFilter.java
+++ src/main/java/org/vinhveer/message/config/JwtAuthenticationFilter.java
@@ -1,4 +1,4 @@
-package org.vinhveer.message.config;
+package org.vinhveer.message.Config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
--- src/main/java/org/vinhveer/message/config/SecurityConfiguration.java
+++ src/main/java/org/vinhveer/message/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package org.vinhveer.message.config;
+package org.vinhveer.message.Config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
