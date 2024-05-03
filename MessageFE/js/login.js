044f8e0f4e17a7084b313f376f65e08cf4f60813
VINHVEER\vinhv <vinhveer123@gmail.com>
2024-05-03 17:26:17 +0700

    [master] Login

---
+++ MessageFE/js/login.js
@@ -0,0 +1,48 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    // Ngăn chặn hành vi mặc định của form
+    event.preventDefault();
+
+    // Lấy dữ liệu từ form
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Tạo đối tượng request
+    const requestData = {
+        email: username,
+        password: password
+    };
+
+    // Thực hiện fetch API
+    fetch('http://localhost:8080/auth/authenticate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+    .then(response => {
+        if (!response.ok || response.status !== 200) {
+            throw new Error('<div class="alert alert-danger" role="alert"><strong>Thông báo!</strong> Email hoặc mật khẩu không đúng.</div>');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data && data.token) {
+            // Lưu token vào Storage
+            localStorage.setItem('token', data.token);
+
+            // Chuyển hướng trang
+            window.location.href = 'app/index.html';
+        } else {
+            // Hiển thị thông báo lỗi
+            document.querySelector('.notification').innerHTML = '<div class="alert alert-danger" role="alert"><strong>Thông báo!</strong> Email hoặc mật khẩu không đúng.</div>';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.querySelector('.notification').innerHTML = error.message;
+
+        // Xóa token khỏi Local Storage
+        localStorage.removeItem('token');
+    });
+});
