f5f0a025b9db1e66adf4cf02cd91e38b39f5b7f0
VINHVEER\vinhv <vinhveer123@gmail.com>
2024-05-03 15:06:10 +0700

    [master] fix MessageAPI

--- MessageAPI/src/main/java/org/vinhveer/message/Config/SecurityConfiguration.java
+++ MessageAPI/src/main/java/org/vinhveer/message/Config/SecurityConfiguration.java
@@ -4,14 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -27,15 +25,11 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/auth/**", "/ws/**", "/login/**", "/css/**", "/js/**")
+                        req.requestMatchers("/auth/**", "/ws/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
                 )
-//                .formLogin(login -> login
-//                        .loginPage("/login")
-//                        .defaultSuccessUrl("/home", true)
-//                )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
--- MessageAPI/src/main/java/org/vinhveer/message/Controller/UserController.java
+++ MessageAPI/src/main/java/org/vinhveer/message/Controller/UserController.java
@@ -1,4 +1,4 @@
-package org.vinhveer.message.RestController;
+package org.vinhveer.message.Controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
