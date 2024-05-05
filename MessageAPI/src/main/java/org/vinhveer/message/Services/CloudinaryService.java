a1ffc21de1e60323519865f6de68e1fef2562638
VINHVEER\vinhv <vinhveer123@gmail.com>
2024-05-05 17:41:08 +0700

    Upload

--- MessageAPI/src/main/java/org/vinhveer/message/Services/CloudinaryService.java
+++ MessageAPI/src/main/java/org/vinhveer/message/Services/CloudinaryService.java
@@ -1,2 +1,9 @@
-package org.vinhveer.message.Services;public interface CloudinaryService {
+package org.vinhveer.message.Services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface CloudinaryService {
+    public String uploadFile(MultipartFile file);
 }
