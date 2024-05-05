a6ea5bde51e01c4ab1d2579343401d0a3b5506c0
VINHVEER\vinhv <vinhveer123@gmail.com>
2024-05-05 10:45:21 +0700

    Upload

--- MessageAPI/src/main/java/org/vinhveer/message/Config/MessageWebSocketHandler.java
+++ MessageAPI/src/main/java/org/vinhveer/message/Config/MessageWebSocketHandler.java
@@ -1,29 +1,32 @@
 package org.vinhveer.message.Config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.vinhveer.message.Entity.Message;
+import org.vinhveer.message.Services.MessageService;
 
 @Component
 public class MessageWebSocketHandler extends TextWebSocketHandler {
+    private final MessageService messageService;
+    private final ObjectMapper objectMapper;
+
+    public MessageWebSocketHandler(MessageService messageService, ObjectMapper objectMapper) {
+        this.messageService = messageService;
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("WebSocket connection established: " + session.getId());
     }
 
     @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        super.handleMessage(session, message);
-    }
-
-    @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        super.handleTextMessage(session, message);
-    }
-
-    @Override
-    protected void handlePongMessage(WebSocketSession session, PongMessage message) throws Exception {
-        super.handlePongMessage(session, message);
+        Message msg = objectMapper.readValue(message.getPayload(), Message.class);
+        messageService.sendMessage(msg);
+        session.sendMessage(new TextMessage("Message sent successfully"));
     }
 
     @Override
@@ -34,10 +37,5 @@ public class MessageWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
-    }
-
-    @Override
-    public boolean supportsPartialMessages() {
-        return super.supportsPartialMessages();
     }
 }
--- MessageAPI/src/main/java/org/vinhveer/message/Config/WebSocketConfig.java
+++ MessageAPI/src/main/java/org/vinhveer/message/Config/WebSocketConfig.java
@@ -1,16 +1,25 @@
 package org.vinhveer.message.Config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.vinhveer.message.Services.MessageService;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+    private final MessageService messageService;
+    private final ObjectMapper objectMapper;
+
+    public WebSocketConfig(MessageService messageService, ObjectMapper objectMapper) {
+        this.messageService = messageService;
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new MessageWebSocketHandler(), "/ws").setAllowedOrigins("*");
+        registry.addHandler(new MessageWebSocketHandler(messageService, objectMapper), "/ws").setAllowedOrigins("*");
     }
 }
