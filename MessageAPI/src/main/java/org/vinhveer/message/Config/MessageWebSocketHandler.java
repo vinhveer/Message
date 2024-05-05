c6d8922c7a3150e3f2f64b4ff2636a59097ccf7a
VINHVEER\vinhv <vinhveer123@gmail.com>
2024-05-05 11:43:42 +0700

    Test websocket

--- MessageAPI/src/main/java/org/vinhveer/message/Config/MessageWebSocketHandler.java
+++ MessageAPI/src/main/java/org/vinhveer/message/Config/MessageWebSocketHandler.java
@@ -7,10 +7,14 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.vinhveer.message.Entity.Message;
 import org.vinhveer.message.Services.MessageService;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 public class MessageWebSocketHandler extends TextWebSocketHandler {
     private final MessageService messageService;
     private final ObjectMapper objectMapper;
+    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
 
     public MessageWebSocketHandler(MessageService messageService, ObjectMapper objectMapper) {
         this.messageService = messageService;
@@ -20,13 +24,18 @@ public class MessageWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("WebSocket connection established: " + session.getId());
+        sessions.put(session.getId(), session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         Message msg = objectMapper.readValue(message.getPayload(), Message.class);
         messageService.sendMessage(msg);
-        session.sendMessage(new TextMessage("Message sent successfully"));
+        TextMessage response = new TextMessage(objectMapper.writeValueAsString(msg));
+        for (WebSocketSession webSocketSession : sessions.values()) {
+            if (!webSocketSession.getId().equals(session.getId()))
+                webSocketSession.sendMessage(response);
+        }
     }
 
     @Override
@@ -36,6 +45,6 @@ public class MessageWebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        super.afterConnectionClosed(session, status);
+        sessions.remove(session.getId());
     }
 }
