eb7d8c76d99cb6e869c7c9899b63572c2c3aa718
VINHVEER\vinhv <vinhveer123@gmail.com>
2024-05-06 11:35:10 +0700

    Allow all request in CORS

--- MessageAPI/src/main/java/org/vinhveer/message/Config/CORSConfig.java
+++ MessageAPI/src/main/java/org/vinhveer/message/Config/CORSConfig.java
@@ -9,7 +9,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
